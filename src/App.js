2867de53713127e3827f8cce9d20588228ad8e2f
Franklyn Vasquez <franklynvasquez24@gmail.com>
2025-04-24 22:33:21 -0400

    Created personal details editor

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { useReducer } from 'react';
 import generalReducer from './editor/generalReducer';
 import SimpleEditor from './editor/SimpleEditor';
+import Button from 'react-bootstrap/Button'
 import ObjectEditor from './editor/ObjectEditor';
 import { SIMPLE_TYPES, TYPES } from './editor/types';
 
@@ -27,6 +28,55 @@ function App() {
   const [justName, dispatchJustName] = useReducer(generalReducer, '')
   const [justAge, dispatchJustAge] = useReducer(generalReducer, 0)
   const [object, disptachObject] = useReducer(generalReducer, {})
+  const [personalDetails, dispatchPersonalDetails] = useReducer(generalReducer, {
+    first: '' //Can specify default here instead of schema
+  })
+
+  const BASIC_PERSONAL_DETAILS_CHILDREN_SCHEMA = {
+    first: {
+      label: 'First Name',
+      default: '',
+      required: true
+    },
+    last: {
+      label: 'Last Name',
+      default: '',
+      required: true
+    },
+    age: {
+      label: 'Age',
+      default: 0,
+      required: true
+    }
+  }
+
+  const PERSONAL_DETAILS_SCHEMA = {
+    label: "Personal Details",
+    dispatcher: dispatchPersonalDetails,
+    all: {
+      types: [] //types of all children are restricted to no change
+    },
+    children: {
+      ...BASIC_PERSONAL_DETAILS_CHILDREN_SCHEMA,
+      friends: {
+        label: 'Friends',
+        default: [],
+        required: true,
+        others: {
+          types: ['object'],
+          children: BASIC_PERSONAL_DETAILS_CHILDREN_SCHEMA
+        }
+      },
+      extra: {
+        label: 'Extra',
+        default: {},
+        required: true,
+        others: {
+          types: ['string']
+        }
+      }
+    }
+  }
 
   return (
     <div className="App">
@@ -105,6 +155,17 @@ function App() {
               generator: createFreeGenerator(true)
             }}
           />
+          <hr />
+        </div>
+      }
+      {
+        true &&
+        <div>
+          <ObjectEditor
+            value={personalDetails}
+            schema={PERSONAL_DETAILS_SCHEMA}
+          />
+          <Button onClick={() => console.log(personalDetails)}>Print Personal Details To Console</Button>
           <hr />
         </div>
       }
